39b490c502e99adf785f70bcde9faae9fbc1d8e4
ifejeremiah <jerryroboy.16@gmail.com>
2021-02-04 18:18:57 +0100

    Game Code

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const character = document.querySelector('#character');
 const block = document.querySelector('#block');
 
-function jump(){
-    if (character.classList != 'animate'){
+function jump() {
+    if (character.classList != 'animate') {
         character.classList.add('animate');
     }
-    
-    setTimeout(function(){
+
+    setTimeout(function () {
         character.classList.remove('animate');
     }, 500)
 }
 
-const checkDead = setInterval(function() {
-    const characterTop = 
-    parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+const checkDead = setInterval(function () {
+    const characterTop =
+        parseInt(window.getComputedStyle(character).getPropertyValue('top'));
 
-    const blockLeft = 
-    parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    const blockLeft =
+        parseInt(window.getComputedStyle(block).getPropertyValue('left'));
 
-    if( blockLeft > 0 && blockLeft < 85 && characterTop >= 98){
+    if (blockLeft < 20 && characterTop >= 130) {
         block.style.animation = 'none';
         block.style.display = 'none';
         alert('You lose!\nTry again.');
     }
 }, 10);
+
